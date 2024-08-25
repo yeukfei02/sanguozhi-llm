bd6cf2cf1095ad382ed2edb437f86c21492ffd85
Donald Wu <yeukfei02@gmail.com>
2024-08-25 15:01:23 +0800

    fix: add .env and fix sh file

--- push-to-ecr.sh
+++ push-to-ecr.sh
@@ -1,19 +1,25 @@
 #! /bin/zsh
 
-AWS_ACCOUNT_ID=""
-AWS_REGION="ap-southeast-1"
-IMAGE_NAME="sanguozhi-llm-api"
-ECR_REPOSITORY="sanguozhi-llm-api"
-TAG="latest"
+source .env
 
-# build docker image
-docker build -t $IMAGE_NAME .
+echo "AWS_ACCOUNT_ID = $AWS_ACCOUNT_ID"
 
-# login to aws ecr
-aws ecr get-login-password --region $AWS_REGION --profile convo | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+if [ ! -z "$AWS_ACCOUNT_ID" ]; then
+    AWS_ACCOUNT_ID="$AWS_ACCOUNT_ID"
+    AWS_REGION="ap-southeast-1"
+    IMAGE_NAME="sanguozhi-llm-api"
+    ECR_REPOSITORY="sanguozhi-llm-api"
+    TAG="latest"
 
-# docker tag
-docker tag $IMAGE_NAME:$TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:$TAG
+    # build docker image
+    docker build -t $IMAGE_NAME .
 
-# push to ecr
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:$TAG
+    # login to aws ecr
+    aws ecr get-login-password --region $AWS_REGION --profile convo | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+    # docker tag
+    docker tag $IMAGE_NAME:$TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:$TAG
+
+    # push to ecr
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:$TAG
+fi
